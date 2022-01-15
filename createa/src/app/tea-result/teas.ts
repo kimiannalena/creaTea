3bf696a8309d94f289e2dda8d52cb4f31600e070
Kimberly Munjal <kimi@Kimi.fritz.box>
2022-01-15 13:58:09 +0100

    Add more tea content to json

--- createa/src/app/tea-result/teas.ts
+++ createa/src/app/tea-result/teas.ts
@@ -19,4 +19,58 @@ export const teas: Tea[] = [
         description: 'iwas',
         ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
     },
+    {
+        id: 4,
+        title: 'Frühling',
+        description: 'iwas',
+        ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
+    },
+    {
+        id: 5,
+        title: 'Sommer',
+        description: 'iwas',
+        ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
+    },
+    {
+        id: 6,
+        title: 'Herbst',
+        description: 'iwas',
+        ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
+    },
+    {
+        id: 7,
+        title: 'Winter',
+        description: 'iwas',
+        ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
+    },
+    {
+        id: 8,
+        title: 'Wachmacher',
+        description: 'iwas',
+        ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
+    },
+    {
+        id: 9,
+        title: 'Vor dem Sport',
+        description: 'iwas',
+        ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
+    },
+    {
+        id: 10,
+        title: 'Abendwonne',
+        description: 'iwas',
+        ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
+    },
+    {
+        id: 11,
+        title: 'Work-Life-Balance',
+        description: 'iwas',
+        ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
+    },
+    {
+        id: 12,
+        title: 'Filmeabend',
+        description: 'iwas',
+        ingredients: ['Schwarztee', 'Ingwer', 'Zitrone']
+    },
 ]
